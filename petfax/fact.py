3cc178e18f9e987bf18fc5d9fc0a894d76ebee32
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-06-15 18:35:59 -0500

    adding /facts/ with GET & POST, creating facts/index.html

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,4 +1,4 @@
-from flask import ( Blueprint, render_template ) 
+from flask import ( Blueprint, render_template, request, redirect ) 
 
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
@@ -6,3 +6,13 @@ bp = Blueprint('fact', __name__, url_prefix="/facts")
 @bp.route('/new')
 def new(): 
     return render_template('facts/new.html')
+
+@bp.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        print(request.form)
+        return redirect('/facts')
+    
+    # returns a redirect to /facts/ - @bp.route('/') to leave the if statement then renders facts/index.html
+    # if this was a GET request it would not enter the if statement logic
+    return render_template('facts/index.html')
