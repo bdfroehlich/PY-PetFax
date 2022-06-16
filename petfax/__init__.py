89fbc39632406bdf12906a8f7ed1be4d332d3645
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-06-15 19:58:51 -0500

    install SQLAlchemy, Flask-Migrate, psycopg2, setup model and migrate

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,9 +1,18 @@
 # config                    
 from flask import Flask
+from flask_migrate import Migrate
 
 # factory
 def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:benj4min21@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False             
+
+    from . import models
+    models.db.init_app(app)
+    #pass the imported Migrate class the Flask app instance and the SQLAlchemy db instance from models.py
+    migrate = Migrate(app, models.db)
 
     # index route
     @app.route('/')
