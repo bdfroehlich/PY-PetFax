69b83f56438fe8d2e5ffc721ab3f867f6aa0dbeb
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-06-15 20:50:23 -0500

    interacting with database

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,4 +1,5 @@
 from flask import ( Blueprint, render_template, request, redirect ) 
+from . import models
 
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
@@ -10,9 +11,20 @@ def new():
 @bp.route('/', methods=['GET','POST'])
 def index():
     if request.method == 'POST':
-        print(request.form)
+        submitter = request.form['submitter']
+        fact = request.form['fact']
+
+        new_fact = models.Fact(submitter=submitter,fact=fact)
+        #add new_fact to database session
+        models.db.session.add(new_fact)
+        #commit the database session
+        models.db.session.commit()
         return redirect('/facts')
+
+    results = models.Fact.query.all()
+    # for result in results:
+    #     print(result)
     
     # returns a redirect to /facts/ - @bp.route('/') to leave the if statement then renders facts/index.html
     # if this was a GET request it would not enter the if statement logic
-    return render_template('facts/index.html')
+    return render_template('facts/index.html', facts=results)
